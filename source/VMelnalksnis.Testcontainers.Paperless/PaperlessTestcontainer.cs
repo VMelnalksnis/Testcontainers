cece0dd4ecedfd567c3b5714a9bcd0a311b682eb
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-11-13 19:25:52 +0200

    feat: Add PaperlessTestcontainer

---
+++ source/VMelnalksnis.Testcontainers.Paperless/PaperlessTestcontainer.cs
@@ -0,0 +1,67 @@
+﻿// Copyright 2022 Valters Melnalksnis
+// Licensed under the Apache License 2.0.
+// See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+
+using JetBrains.Annotations;
+
+using Microsoft.Extensions.Logging;
+
+namespace VMelnalksnis.Testcontainers.Paperless;
+
+/// <summary>An extended configured <see cref="HostedServiceContainer"/> for paperless.</summary>
+public sealed class PaperlessTestcontainer : HostedServiceContainer
+{
+	private const int _defaultPaperlessPort = 8000;
+
+	[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+	private PaperlessTestcontainer(ITestcontainersConfiguration configuration, ILogger logger)
+		: base(configuration, logger)
+	{
+	}
+
+	/// <summary>Gets the base address of the paperless instance.</summary>
+	/// <returns>The base address of the paperless instance.</returns>
+	public Uri GetBaseAddress() => new($"http://localhost:{GetMappedPublicPort(_defaultPaperlessPort)}/");
+
+	/// <summary>Creates a new API token for the default admin user.</summary>
+	/// <returns>A new API token for the default admin user.</returns>
+	/// <exception cref="HttpRequestException">The request to create the token failed.</exception>
+	/// <exception cref="InvalidDataException">The request was successful, but did not contain a token.</exception>
+	public async Task<string> GetAdminToken()
+	{
+		var httpClient = new HttpClient();
+		httpClient.BaseAddress = GetBaseAddress();
+
+		var loginContent = new FormUrlEncodedContent(new List<KeyValuePair<string?, string?>>
+		{
+			new("username", Username),
+			new("password", Password),
+		});
+
+		var response = await httpClient.PostAsync("/api/token/", loginContent).ConfigureAwait(false);
+		var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+		if (response.StatusCode is not HttpStatusCode.OK)
+		{
+#if NET7_0_OR_GREATER
+			throw new HttpRequestException(content, null, response.StatusCode);
+#else
+			throw new HttpRequestException(content);
+#endif
+		}
+
+		var responseJson = JsonNode.Parse(content);
+		return responseJson?["token"]?.GetValue<string>() ??
+			throw new InvalidDataException("Response did not contain a token");
+	}
+}
---
+++ tests/VMelnalksnis.Testcontainers.Paperless.Tests/PaperlessTestcontainerTests.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2022 Valters Melnalksnis
+// Licensed under the Apache License 2.0.
+// See LICENSE file in the project root for full license information.
+
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+
+namespace VMelnalksnis.Testcontainers.Paperless.Tests;
+
+public sealed class PaperlessTestcontainerTests : IAsyncLifetime
+{
+	private readonly RedisTestcontainer _redis;
+
+	public PaperlessTestcontainerTests()
+	{
+		var redisConfiguration = new RedisTestcontainerConfiguration("docker.io/library/redis:7.0.5");
+		_redis = new TestcontainersBuilder<RedisTestcontainer>()
+			.WithDatabase(redisConfiguration)
+			.Build();
+	}
+
+	public Task InitializeAsync() => _redis.StartAsync();
+
+	[Fact]
+	public async Task LoginPageShouldWork()
+	{
+		var paperlessConfiguration = new PaperlessTestcontainerConfiguration();
+		await using var paperless = new TestcontainersBuilder<PaperlessTestcontainer>()
+			.WithPaperless(paperlessConfiguration, _redis)
+			.Build();
+
+		await paperless.StartAsync();
+
+		var token = await paperless.GetAdminToken();
+		using var httpClient = new HttpClient();
+		httpClient.BaseAddress = paperless.GetBaseAddress();
+		httpClient.DefaultRequestHeaders.Authorization = new("Token", token);
+		var response = await httpClient.GetAsync("/accounts/login/?next=/");
+
+		using (new AssertionScope())
+		{
+			response.StatusCode.Should().Be(HttpStatusCode.OK);
+			response.Content.Headers.ContentType?.MediaType.Should().Be(MediaTypeNames.Text.Html);
+			response.Content.Headers.ContentType?.CharSet.Should().Be("utf-8");
+		}
+	}
+
+	public Task DisposeAsync() => _redis.DisposeAsync().AsTask();
+}
